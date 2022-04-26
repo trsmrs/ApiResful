2cae73bec5a43311577adb9df12e6ad0b7863f6e
Tiago <trsmtiago@gmail.com>
2022-04-26 14:21:45 -0300

    adicionando o metodo get(restful)

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -6,8 +6,7 @@ async function get(req, res){
    const { id } = req.params
 
     const obj = id ? { _id: id } : null
-
-   const products = await ProductsModel.find(obj)
+    const products = await ProductsModel.find(obj)
  
  
     res.send(products)
